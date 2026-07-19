82cc9fbb280109d62ebeb4b33760648ece403a52
najimilad2345-ui <najimilad2345@gmail.com>
2026-07-20 02:37:15 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 
+// Takhte Nard Game
+console.log("🎲 Takhte Nard Started");
+
+function rollDice() {
+    const dice1 = Math.floor(Math.random() * 6) + 1;
+    const dice2 = Math.floor(Math.random() * 6) + 1;
+
+    alert("🎲 تاس اول: " + dice1 + "\n🎲 تاس دوم: " + dice2);
+}
